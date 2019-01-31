f134deb806d94bf3a29689ee8a75b5112a97972f
Jack Phillips <jackwphillips@Jacks-MBP.verizon.net>
2019-01-31 13:57:32 -0500

    Add markers on map

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -1,9 +1,11 @@
 ({
     doInit : function(component, event, helper) {
-        console.log("Init for AccountList called.");
         let action = component.get("c.findAll");
         action.setCallback(this, function(a) {
             component.set("v.accounts", a.getReturnValue());
+            var event = $A.get("e.c:AccountsLoaded");
+            event.setParams({"accounts":a.getReturnValue()});
+            event.fire();
         });
         $A.enqueueAction(action);
     }
